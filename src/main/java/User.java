f03955b7f2743189e526d062530df4e4ae89d497
RandyZhang1 <1766920049@qq.com>
2018-03-09 16:19:57 +0800

    添加一个姓名

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,5 +1,14 @@
 public class User {
     private int id;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public int getId() {
         return id;
